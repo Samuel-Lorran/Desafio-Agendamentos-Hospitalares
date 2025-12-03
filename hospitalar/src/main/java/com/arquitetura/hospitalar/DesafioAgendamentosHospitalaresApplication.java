d707d9f72bd6bece1f3e67ec408ba3e7c165296c
Samuel Lorran <samuellorran.ti@gmail.com>
2025-12-03 19:17:21 -0300

    Codigo Atualizado

---
+++ hospitalar/src/main/java/com/arquitetura/hospitalar/DesafioAgendamentosHospitalaresApplication.java
@@ -0,0 +1,13 @@
+package com.arquitetura.hospitalar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioAgendamentosHospitalaresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioAgendamentosHospitalaresApplication.class, args);
+	}
+
+}
